c29dbad2e2ecb89d145e2ef980433864189a307f
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-07-01 13:26:12 +0100

    Create MenuItem model class

---
+++ lib/models/menu_item.dart
@@ -0,0 +1,8 @@
+class MenuItem {
+  final String category;
+  final String imageUrl;
+  final String name;
+  final int price;
+
+  MenuItem({this.category, this.imageUrl, this.name, this.price});
+}
